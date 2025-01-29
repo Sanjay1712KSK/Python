4b5db185fc6da0607d17c1adc0938d744aa49e85
Sanjay1712KSK <sanjayksk1712@gmail.com>
2025-01-30 00:39:26 +0530

    Updated

--- p2.py
+++
@@ -1,1 +0,0 @@
-print("SANJAY KUMAR")
--- twc.py
+++ twc.py
@@ -1,7 +1,8 @@
+import os
 def t(msg,num):
     from twilio.rest import Client
-    account_sid = '<yourid>'  
-    auth_token = '<yourtoken>'  
+    account_sid = os.getenv('TWILIO_ACCOUNT_SID') 
+    auth_token = os.getenv('TWILIO_AUTH_TOKEN')
     client = Client(account_sid, auth_token)
     message = client.messages.create(
         body=msg,  
